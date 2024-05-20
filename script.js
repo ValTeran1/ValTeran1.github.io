5f569c6c5df717cb8fc8c6e50363cbcf78c1f28e
ValTeran1 <terananavaleria1@gmail.com>
2024-05-20 13:37:04 -0600

    initial commit again

---
+++ script.js
@@ -0,0 +1,11 @@
+function cargarContenido(archivo) {
+    fetch(archivo)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('contenido').innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error al cargar el contenido:', error);
+      });
+  }
+  
